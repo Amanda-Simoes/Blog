e8ce2900dfd9953190e6597fe30047148642303a
Amanda <amanda.simoes1707@gmail.com>
2021-06-10 19:41:17 -0300

    Conexão com o banco MySQL

---
+++ database/database.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize")
+require('dotenv').config()
+
+const connection = new Sequelize(process.env.MYSQL_DB,process.env.MYSQL_ROOT,process.env.MYSQL_PASSWORD,{ 
+    host: "localhost",
+    dialect: "mysql"
+})
+
+
+module.exports = connection
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
+const { urlencoded } = require("body-parser")
+const connection = require("./database/database")
 
-app.get("/",(req,res) => {
-    res.send("Bem vindo ao meu blog")
+// View engine
+app.set('view engine','ejs')
+
+// Static
+app.use(express.static('public'))
+
+// Body-parser
+app.use(urlencoded({extended: false}))
+app.use(express.json())
+
+// Connection
+connection.authenticate().then(() => {
+    console.log("Conexão feita com sucesso")
+}).catch((error) => {
+    console.log(error)
 })
 
+// Route
+app.get("/",(req,res) => {
+    res.render("index")
+})
+
+// Port
 app.listen(2000, () => {
     console.log("O servidor está ativo!")
 })
